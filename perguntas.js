8699deda97be53dd96b8c29fbb317f95a5e90f40
Rodrigo Tataruk <caldas.rodrigo@escola.pr.gov.br>
2025-10-20 23:51:59 +0000

    alteracao

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,75 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
+    'Programação'
+    'O que é Python?'
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da Alemanha?',
+    'Qual a capital da Alemanha?'
     'A capital da Alemanha é Berlim'
 )
 
 criaCartao(
     'História',
-    'Quem criou a CLT (consolidação das leis do trabalho)?',
+    'Quem criou a CLT (consolidação das leis do trabalho)?'
     'Getúlio Vargas'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
+    'Lingua inglesa'
+    'Como se diz oi em Inglês?'
     'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-    'Matemática',
-    'Oque é a Hipotenuza?',
+    'Matemática'
+    'Oque é a Hipotenuza?'
     'O maior lado de um triângulo retângulo'
 )
 
 criaCartao(
     'Biologia',
-    'Oque significa a sigla DNA?',
+    'Oque significa a sigla DNA?'
     'Ácido Desoxirribonucleico'
 )
 
 criaCartao(
-    'Ed. Financeira',
-    'Oque é Bitcoin?',
+    'Ed. Financeira'
+    'Oque é Bitcoin?'
     'Uma criptomoeda'
 )
 
 criaCartao(
     'Filosofia',
-    'Que filósofo é conhecido pela alegoria filosófica "Mito da Caverna"?',
+    'Que filósofo é conhecido pela alegoria filosófica "Mito da Caverna"?'
     'Platão'
 )
 
 criaCartao(
     'Física',
-    'Qual a primeira lei de Newton',
+    'Qual a primeira lei de Newton'
     'Lei da Inércia'
 )
+
+criaCartao(
+    'Robótica (Física 1)'
+    'O que é o Arduino?'
+    'Arduino é uma placa que você pode programar para controlar luzes, sensores e motores'
+)
+
+criaCartao(
+    'Matemática 1'
+    'Oque é Marketing?'
+    'Marketing é divulgar e vender produtos ou ideias'
+)
+
+criaCartao(
+    'Biologia 1',
+    'Qual é a segunda lei de Mendel?'
+    'A segunda lei de Mendel é a Lei da Segregação Independente'
+)
+
+
+
+
