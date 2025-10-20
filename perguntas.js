ba42cbde19d8a8a2270bef88991541030a804e3d
Rodrigo Tataruk <caldas.rodrigo@escola.pr.gov.br>
2025-10-20 23:38:30 +0000

    a

--- perguntas.js
+++ perguntas.js
@@ -6,18 +6,48 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'História',
+    'Quem criou a CLT (consolidação das leis do trabalho)?',
+    'Getúlio Vargas'
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Oque é a Hipotenuza?',
+    'O maior lado de um triângulo retângulo'
+)
+
+criaCartao(
+    'Biologia',
+    'Oque significa a sigla DNA?',
+    'Ácido Desoxirribonucleico'
+)
+
+criaCartao(
+    'Ed. Financeira',
+    'Oque é Bitcoin?',
+    'Uma criptomoeda'
+)
+
+criaCartao(
+    'Filosofia',
+    'Que filósofo é conhecido pela alegoria filosófica "Mito da Caverna"?',
+    'Platão'
+)
+
+criaCartao(
+    'Física',
+    'Qual a primeira lei de Newton',
+    'Lei da Inércia'
 )
